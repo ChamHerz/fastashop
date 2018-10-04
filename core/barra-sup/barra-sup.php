2088b7a15c4e1576487246269724ce343f6dd40a
ChamHerz <adrielchambers@gmail.com>
2018-10-04 02:01:18 -0300

    inicio

---
+++ core/barra-sup/barra-sup.php
@@ -0,0 +1,27 @@
+<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+	<a class="navbar-brand" href="#">FastaShop</a>
+	<button class="navbar-toggler" type="button" data-toggle="collapse"
+		data-target="#navbarsExample04" aria-controls="navbarsExample04"
+		aria-expanded="false" aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+	</button>
+
+	<div class="collapse navbar-collapse" id="navbarsExample04">
+		<ul class="navbar-nav mr-auto">
+			<li class="nav-item"><a class="nav-link" href="#">Registrarse</a></li>
+			<li class="nav-item"><a class="nav-link" href="#">Login</a>
+			</li>
+			<li class="nav-item dropdown"><a class="nav-link dropdown-toggle"
+				href="https://example.com" id="dropdown04" data-toggle="dropdown"
+				aria-haspopup="true" aria-expanded="false">Acerca de</a>
+				<div class="dropdown-menu" aria-labelledby="dropdown04">
+					<a class="dropdown-item" href="#">Programacion C</a> <a
+						class="dropdown-item" href="#">Php Version</a> <a
+						class="dropdown-item" href="#">MySQL Version</a>
+				</div></li>
+		</ul>
+		<form class="form-inline my-2 my-md-0">
+			<input class="form-control" type="text" placeholder="Buscar Producto">
+		</form>
+	</div>
+</nav>
---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE unspecified PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport"
+	content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>FastaShop</title>
+	<link rel="stylesheet" href="lib/bootstrap.min.css">
+</head>
+<body>
+	<div class="container-fluid">
+		<?php include 'core/barra-sup/barra-sup.php'; ?>
+	</div>
+	<script type="text/javascript" src="lib/jquery-3.3.1.min.js"></script>
+	<script type="text/javascript" src="lib/bootstrap.min.js"></script>
+</body>A
+</html>
+<?php
+// phpinfo();
+?>
